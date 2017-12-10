ebd67d0c9e485af856a88fbc9bc93dff62d73938
Eric <Eric@ERIC_WPAD>
2017-12-11 00:35:29 +0800

    透過Entity Framework操作CRUD動作

--- MVC5Course/Controllers/TestController.cs
+++ MVC5Course/Controllers/TestController.cs
@@ -19,5 +19,64 @@ namespace MVC5Course.Controllers
                        select p;
             return View(data.Take(10));
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product data)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Product.Add(data);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(data);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var item = db.Product.Find(id);
+            return View(item);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, Product data)
+        {
+            if(ModelState.IsValid)
+            {
+                var item = db.Product.Find(id);
+
+                item.ProductName = data.ProductName;
+                item.Price = data.Price;
+                item.Active = data.Active;
+                item.Stock = data.Stock;
+
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(data);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(db.Product.Find(id));
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var item = db.Product.Find(id);
+
+            db.OrderLine.RemoveRange(item.OrderLine.ToList());
+            db.Product.Remove(item);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
