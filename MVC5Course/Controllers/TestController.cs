948b868dc66cd126e0974c5399a3f6bacf419342
Eric <Eric@ERIC_WPAD>
2017-12-10 23:47:49 +0800

    利用Empty Controller建立TestController，並建立Index頁面

---
+++ MVC5Course/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using MVC5Course.Models;
+
+namespace MVC5Course.Controllers
+{
+    public class TestController : Controller
+    {
+        FabricsEntities db = new FabricsEntities();
+
+        // GET: Test
+        public ActionResult Index()
+        {
+            var data = from p in db.Product
+                       select p;
+            return View(data.Take(10));
+        }
+    }
+}
