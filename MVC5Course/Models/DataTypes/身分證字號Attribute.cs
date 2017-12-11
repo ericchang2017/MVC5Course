660c9d326e747eefcf668b2e0f652e44b4c92222
Eric <Eric@ERIC_WPAD>
2017-12-12 00:18:41 +0800

    自訂輸入驗證與模型驗證

---
+++ MVC5Course/Models/DataTypes/身分證字號Attribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC5Course.Models.DataTypes
+{
+    public class 身分證字號Attribute : DataTypeAttribute
+    {
+        public 身分證字號Attribute() : base(DataType.Text)
+        {
+
+        }
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return true;
+            }
+            string str = (string)value;
+            if (str.Contains("Eric"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
